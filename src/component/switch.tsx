7361343b068f4641b4a3ab78a8e0139233933c12
zenght <zenght>
2020-05-24 22:13:49 +0800

    feature:重构search&checkbox

--- src/component/switch.tsx
+++ src/component/switch.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ChangeEvent, useState, useEffect } from 'react';
+import React, { ChangeEvent, useState, useEffect } from 'react';
 
 interface SwitchProps {
     leftlabel?: any,
@@ -69,32 +69,3 @@ export default function Switch(props: SwitchProps): JSX.Element {
         </div>
     );
 }
-
-// export default class Switch extends Component<IProps>{
-//     id: string
-//     constructor(props: IProps) {
-//         super(props);
-//         this.id = Math.random().toString(36).substring(2);
-//     }
-//     render() {
-//         return (
-//             <div className="mb32 d-f ac">
-//                 <span className='pr8'>{this.props.leftlabel || ''}</span>
-//                 <div className="switch-box-normalize">
-//                     <label htmlFor={this.id} className="switch-mark-click"></label>
-//                     <input
-//                         type="checkbox"
-//                         id={this.id}
-//                         defaultChecked={this.props.defaultChecked}
-
-//                         onChange={(e) => this.props.onChange(e)} />
-//                     <div>
-//                         <i className="switch-move"></i>
-//                         <span className="switch-horizon"></span>
-//                     </div>
-//                 </div>
-//                 <span className='pl8'>{this.props.rightlabel || ''}</span>
-//             </div>
-//         );
-//     }
-// }
